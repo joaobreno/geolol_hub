07668c241bc97eddf0c2aea6c8c073301752d23f
João Breno <jbrenopamplona@gmail.com>
2024-01-22 23:59:34 -0400

    primeiro redirect para login para profile e separacao partials

--- server_geolol/home/views.py
+++ server_geolol/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.urls import *
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
+@login_required
+def profile(request):
+    return render(request, 'users-profile.html')
