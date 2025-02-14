8b40904b018285260202ecf5d2a56581266f3076
Joao Breno Pamplona <brenopamplona@hotmail.com>
2025-02-14 08:09:14 -0400

    implementacao Docker

--- server_geolol/geolol_hub/settings.py
+++ server_geolol/geolol_hub/settings.py
@@ -174,6 +174,10 @@ ICON_SPELL_DICT = {
     55: 'Summoner_UltBookSmitePlaceholder',
 }
 
+# Redis configs
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_HOST_PORT = os.getenv("REDIS_HOST_PORT", "6379")
+REDIS_ENABLED = os.getenv("REDIS_ENABLED", "False").lower() == "true"
 
 ### CELERY
 CELERY_TIMEZONE = 'America/Sao_Paulo'
@@ -181,3 +185,4 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND =  os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
