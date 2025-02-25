f9c60b116ec475b6f99dce5b69a85f91784f86fb
João Breno <jbrenopamplona@gmail.com>
2025-02-24 22:24:13 -0400

    implantacao preloader

--- server_geolol/geolol_hub/static/landpage_assets/js/main.js
+++ server_geolol/geolol_hub/static/landpage_assets/js/main.js
@@ -8,6 +8,13 @@
 (function() {
   "use strict";
 
+  $(window).on('load', function () {
+    $('#preloader-active').delay(450).fadeOut('slow');
+    $('body').delay(450).css({
+      'overflow': 'visible'
+    });
+  });
+
   /**
    * Easy selector helper function
    */
